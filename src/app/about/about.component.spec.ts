62f07be385354987dc4cbbfbb3468fe7aa8efc47
ronixclh <ronalds.borisovs@inbox.lv>
2024-08-16 19:22:43 +0200

    edit fix

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AboutComponent } from './about.component';
 
 describe('AboutComponent', () => {
@@ -8,9 +7,8 @@ describe('AboutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AboutComponent]
-    })
-    .compileComponents();
+      imports: [AboutComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(AboutComponent);
     component = fixture.componentInstance;
